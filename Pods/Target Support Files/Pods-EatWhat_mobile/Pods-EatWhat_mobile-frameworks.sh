ec66725959ddd78f3b1187d1152f2e48a4eace86
peoplewhite <shonshon7@gmail.com>
2016-01-27 15:43:41 +0800

    [change] remove AFNetworking from cocoapod

--- Pods/Target Support Files/Pods-EatWhat_mobile/Pods-EatWhat_mobile-frameworks.sh
+++ Pods/Target Support Files/Pods-EatWhat_mobile/Pods-EatWhat_mobile-frameworks.sh
@@ -84,10 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-EatWhat_mobile/AFNetworking.framework"
   install_framework "Pods-EatWhat_mobile/Alamofire.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-EatWhat_mobile/AFNetworking.framework"
   install_framework "Pods-EatWhat_mobile/Alamofire.framework"
 fi
